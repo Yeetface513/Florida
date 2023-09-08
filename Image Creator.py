84074c27049f2dd83a9ac8603a32d6d1b2bac19d
Yeetface513 <123550198+Yeetface513@users.noreply.github.com>
2023-09-08 13:44:11 +0100

    adds working code :)

--- Image Creator.py
+++ Image Creator.py
@@ -1,6 +1,15 @@
+import os
+# you need to download the openai library first using python -m pip install openai 
+import openai
+
+PROMPT = input("enter a prompt you want the ai to generate here \n")
+
+openai.api_key = ("enter your api key here") # get your api key from the open ai website
+
 response = openai.Image.create(
-  prompt="a white siamese cat",
-  n=1,
-  size="1024x1024"
+    prompt=PROMPT,
+    n=1,
+    size="256x256",
 )
-image_url = response['data'][0]['url']
+
+print(response["data"][0]["url"]) # go to the url to get your image!
